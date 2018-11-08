c096bd4d4bce89e9e843a4bae74f8f6e3240e5bb
Deni <monco655@gmail.com>
2018-11-08 22:34:12 +0700

    tkinter

--- Caesar.py
+++ Caesar.py
@@ -1,14 +1,16 @@
 from string import ascii_lowercase
+import tkinter as tk
 
+root = tk.Tk()
+frame_encrypt = tk.Frame()
+frame_decipher = tk.Frame()
 alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'*3
 
 def encrypt():
-    key = int(input('Введите ключ для шифрования: '))
 
-    print('Введите сообщение: ')
-    input_string = input().lower()
-
-    output_string = []
+    key = int(key1.get())
+    input_string = input_text1.get(1.0, 'end').lower()
+    output_string = list()
 
     for letter in input_string:
         if letter.isspace():
@@ -18,17 +20,15 @@ def encrypt():
         else:
             output_string.append(letter)
 
-    print('Зашифрованное сообение: {}'. format(''.join(output_string)))
-    return ''.join(output_string)
+    output_text1.delete(1.0, 'end')
+    output_text1.insert(1.0, ''.join(output_string))
 
 
 def decipher():
-    key = int(input('Введите ключ для дешифровки: '))
 
-    print('Введите сообщение: ')
-    input_string = input().lower()
-
-    output_string = []
+    key = int(key2.get())
+    input_string = input_text2.get(1.0, 'end').lower()
+    output_string = list()
 
     for letter in input_string:
         if letter.isspace():
@@ -38,8 +38,33 @@ def decipher():
         else:
             output_string.append(letter)
 
-    print('Encrypted message: {}'.format(''.join(output_string)))
+    output_text2.delete(1.0, 'end')
+    output_text2.insert(1.0, ''.join(output_string))
 
-encrypt()
-decipher()
+subtitle1 = tk.Label(frame_encrypt, text = 'Введите ключ и сообщение для шифрования')
+key1 = tk.Entry(frame_encrypt)
+input_text1 = tk.Text(frame_encrypt, width = '30')
+output_text1 = tk.Text(frame_encrypt, width = '30')
+btn_encrypt1 = tk.Button(frame_encrypt, text = 'Зашифровать', command = encrypt)
 
+subtitle2 = tk.Label(frame_decipher, text = 'Введите ключ и зашифрованнный текст')
+key2 = tk.Entry(frame_decipher)
+input_text2 = tk.Text(frame_decipher, width = '30')
+output_text2 = tk.Text(frame_decipher, width = '30')
+btn_encrypt2 = tk.Button(frame_decipher, text = 'Расшифровать', command = decipher)
+
+frame_encrypt.pack(pady = 10)
+subtitle1.pack()
+key1.pack(pady = 7)
+input_text1.pack(side = 'left')
+output_text1.pack(side = 'right')
+btn_encrypt1.pack(pady = 100, padx = 7)
+
+frame_decipher.pack(pady = 10)
+subtitle2.pack()
+key2.pack(pady = 7)
+input_text2.pack(side = 'left')
+output_text2.pack(side = 'right')
+btn_encrypt2.pack(pady = 100, padx = 7)
+
+root.mainloop()
